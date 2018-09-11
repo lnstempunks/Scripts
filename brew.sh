feff3b0c7cf1ea68ed825f2f2794abfe9f7bfc70
Clayton Gilmer <s893759@stm5m-kcs28345.knoxschools.ad>
2018-09-11 19:14:09 -0400

    Seperate scripts

--- brew.sh
+++ brew.sh
@@ -1,8 +1,3 @@
-echo "Installing Xcode...";
-echo "You may have to go to Self-Service and install the Xcode thing if it asks for an admin password.";
-
-xcode-select --install;
-
 echo "Installing Homebrew...";
 
 git clone https://github.com/Homebrew/brew $HOME/brew;
---
+++ xcode.sh
@@ -0,0 +1,4 @@
+echo "Installing Xcode...";
+echo "You may have to go to Self-Service and install the Xcode thing if it asks for an admin password.";
+
+xcode-select --install;
