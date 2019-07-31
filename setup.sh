c526e8e994834256e5a36a31d68d9949feaaee39
Clayton Gilmer <netsec_planes@outlook.com>
2019-07-31 16:28:40 -0400

    cleaned up scripts

--- setup.sh
+++ setup.sh
@@ -3,8 +3,11 @@ echo "Installing Homebrew...";
 git clone https://github.com/Homebrew/brew $HOME/brew;
 
 echo "[[ -s ~/.bashrc ]] && source ~/.bashrc" >> $HOME/.bash_profile;
-
 echo "PATH=$HOME/brew/bin:$PATH" >> $HOME/.bashrc;
 
 echo "Homebrew installed and path set.";
-echo "Restart Terminal/iTerm to use brew.";
+echo "Installing Xcode..."
+
+xcode-select --install
+
+echo "If you are asked for a password, go to Self-Service and install the Xcode fix."
--- xcode.sh
+++
@@ -1,4 +0,0 @@
-echo "Installing Xcode...";
-echo "You may have to go to Self-Service and install the Xcode thing if it asks for an admin password.";
-
-xcode-select --install;
